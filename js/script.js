bcb2c901f2ef493dec3f81f6760eba3233745661
Trotteriniku <stefanozidda3@gmail.com>
2023-10-31 17:28:01 +0100

    milestone3

--- js/script.js
+++ js/script.js
@@ -4,23 +4,44 @@
     data(){
         return{
             MinecraftTasks: [
-                {
-                    id: 1,
-                    text: 'prendere 10 blocchi di legna',
-                    done: false
-                },
-                {
-                    id: 2,
-                    text: 'prendere 4 blocchi di quarzo ',
-                    done: false
-                },
-                {
-                    id: 3,
-                    text: 'craftare un cane',
-                    done: false
-                },
+                // {
+                //     id: 1,
+                //     text: 'prendere 10 blocchi di legna',
+                //     done: false
+                // },
+                // {
+                //     id: 2,
+                //     text: 'prendere 4 blocchi di quarzo ',
+                //     done: false
+                // },
+                // {
+                //     id: 3,
+                //     text: 'craftare un cane',
+                //     done: false
+                // },
             ],
-            lastId: 3,
+            lastId: 0,
+            todoText: ''
         }
+    },
+    methods:{
+        removeTask(i){
+            console.log(i)
+            this.MinecraftTasks.splice(this.MinecraftTasks[i], 1)
+            
+        },
+        addTask(){
+            this.lastId++;
+            const newTask = {
+                id: this.lastId,
+                text: this.todoText,
+                done: false
+            }
+            this.MinecraftTasks.unshift(newTask);
+            this.todoText = '';
+        },
+    },
+    mounted(){
+        console.log(removeTask);
     }
  }).mount('#app')
