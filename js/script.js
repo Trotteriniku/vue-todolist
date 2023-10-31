1a112de3e5d620dc4f741d2441c7ed7af1714a2f
Trotteriniku <stefanozidda3@gmail.com>
2023-10-31 16:53:44 +0100

    Milestone1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@
  createApp({
     data(){
         return{
-
+            MinecraftTasks: [
+                {
+                    id: 1,
+                    text: 'prendere 10 blocchi di legna',
+                    done: false
+                },
+                {
+                    id: 2,
+                    text: 'prendere 4 blocchi di quarzo ',
+                    done: false
+                },
+                {
+                    id: 3,
+                    text: 'craftare un cane',
+                    done: false
+                },
+            ],
+            lastId: 3,
         }
     }
  }).mount('#app')
